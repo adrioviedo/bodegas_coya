5ac31a0668a00acff1edcfbea15f23c05c2a8828
adrioviedo <UO263569@uniovi.es>
2024-03-27 18:04:20 +0100

    Add new font families to tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      Anton: ['Anton', 'sans-serif'],
+      Roboto: ['Roboto Serif Variable', 'serif']
+    },
     extend: {}
   },
   plugins: []
