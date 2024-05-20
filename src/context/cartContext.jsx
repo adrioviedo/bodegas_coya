b99a86bfbdc41b94feb02ad4cf5a0f7e5974f2c3
adrioviedo <UO263569@uniovi.es>
2024-05-20 22:56:09 +0200

    Improve cart functionality and code structure

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -6,25 +6,24 @@ export const useCart = () => {
   return useContext(CartContext)
 }
 
-export const CartProvider = ({ children }) => {
-  const isBrowser = typeof window !== 'undefined'
+const getInitialCart = () => {
+  if (typeof window !== 'undefined') {
+    return JSON.parse(window.localStorage.getItem('cartItems')) || []
+  }
+  return []
+}
 
-  const [cartItems, setCartItems] = useState(
-    isBrowser ? JSON.parse(window.localStorage.getItem('cartItems')) || [] : []
-  )
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(getInitialCart())
 
   const addToCart = (item) => {
     setCartItems((prevCartItems) => {
-      // check if item is already in cart
       const existingCartItem = prevCartItems.find((cartItem) => cartItem.id === item.id)
-
       if (existingCartItem) {
-        // if item is already in cart, increase its quantity
         return prevCartItems.map((cartItem) =>
           cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
         )
       } else {
-        // if item is not in cart, add it
         return [...prevCartItems, { ...item, quantity: 1 }]
       }
     })
@@ -32,21 +31,16 @@ export const CartProvider = ({ children }) => {
 
   const removeFromCart = (item) => {
     setCartItems((prevCartItems) => {
-      // check if item is in cart
       const existingCartItem = prevCartItems.find((cartItem) => cartItem.id === item.id)
-
       if (existingCartItem) {
-        // if item is in cart and its quantity is more than 1, decrease its quantity
         if (existingCartItem.quantity > 1) {
           return prevCartItems.map((cartItem) =>
             cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
           )
         } else {
-          // if item is in cart and its quantity is 1, remove it
           return prevCartItems.filter((cartItem) => cartItem.id !== item.id)
         }
       } else {
-        // if item is not in cart, return the previous cart items
         return prevCartItems
       }
     })
@@ -54,18 +48,20 @@ export const CartProvider = ({ children }) => {
 
   const clearCart = () => {
     setCartItems([])
-    if (isBrowser) {
+    if (typeof window !== 'undefined') {
       window.localStorage.setItem('cartItems', JSON.stringify([]))
     }
   }
+
   const getCartTotal = () => {
     return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2)
   }
+
   useEffect(() => {
-    if (isBrowser) {
+    if (typeof window !== 'undefined') {
       window.localStorage.setItem('cartItems', JSON.stringify(cartItems))
     }
-  }, [cartItems, isBrowser])
+  }, [cartItems])
 
   return (
     <CartContext.Provider
