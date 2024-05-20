5713a91a286828f51ced3e2678397cabef15d772
adrioviedo <UO263569@uniovi.es>
2024-05-20 22:58:10 +0200

    Update search form submission logic and improve styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,15 +5,17 @@ export const Search = ({ formClass, inputClass }) => {
   const [query, setQuery] = useState('')
 
   const handleSubmit = (event) => {
-    const { query } = Object.fromEntries(new window.FormData(event.target))
-    if (!query) {
-      event.preventDefault()
-    }
+    event.preventDefault() // Prevenir el envío por defecto del formulario
+    const formData = new FormData(event.target)
+    const searchQuery = formData.get('search')
+    if (!searchQuery) return // Si el query está vacío, no hacer nada
+    window.location.href = `/productos?search=${searchQuery}`
   }
+
   const handleChange = (event) => {
     const newQuery = event.target.value
     if (newQuery.startsWith(' ')) return
-    setQuery(event.target.value)
+    setQuery(newQuery)
   }
 
   return (
@@ -24,9 +26,9 @@ export const Search = ({ formClass, inputClass }) => {
         type="text"
         name="search"
         placeholder="Buscar"
-        className={` ${inputClass}`}
+        className={inputClass}
       />
-      <button type="submit" className="hover:scale-125 transition ">
+      <button type="submit" className="hover:scale-125 transition">
         <Magnifier clase="w-4 h-4 md:w-6 md:h-6" />
       </button>
     </form>
