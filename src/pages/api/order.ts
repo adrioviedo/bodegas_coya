3e1dbccbd6d8c4a3e35d41aadcf408706da691fa
adrioviedo <UO263569@uniovi.es>
2024-05-20 22:54:33 +0200

    refactor: Improve error handling and validation in order API endpoint

--- src/pages/api/order.ts
+++ src/pages/api/order.ts
@@ -2,56 +2,62 @@ import type { APIRoute } from "astro";
 import { supabase } from "../../lib/supabase";
 
 export const POST: APIRoute = async ({ request, cookies }) => {
-  const order = await request.json();
+  try {
+    const order = await request.json();
 
-  if (!order) {
-    return new Response("Pedido no recibido", {
-      status: 400,
+    if (!order || !order.cart || !order.total) {
+      return new Response("Pedido no válido", {
+        status: 400,
+      });
+    }
+
+    // Extraer el user_id del token
+    const token = cookies.get("sb-access-token")?.value;
+    if (!token) {
+      return new Response("No autorizado", {
+        status: 401,
+      });
+    }
+
+    const { data: user, error: userError } = await supabase.auth.getUser(token);
+
+    if (userError) {
+      return new Response("Error al autenticar el usuario", {
+        status: 500,
+      });
+    }
+
+    if (!user) {
+      return new Response("Usuario no encontrado", {
+        status: 404,
+      });
+    }
+
+    // Insertar el pedido en la base de datos
+    const { data, error: orderError } = await supabase.from("orders").insert([
+      {
+        user_id: user.user.id,
+        date: new Date(),
+        total: order.total,
+        items: order.cart,
+      },
+    ]);
+
+    if (orderError) {
+      return new Response(
+        `Error al registrar el pedido: ${orderError.message}`,
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return new Response("Pedido registrado", {
+      status: 201,
     });
-  }
-
-  // sacar el user_id del token
-  const token = cookies.get("sb-access-token")?.value;
-  if (!token) {
-    return new Response("No autorizado", {
-      status: 401,
-    });
-  }
-  const { data: user, error } = await supabase.auth.getUser(token);
-
-  if (error) {
-    return new Response("User Fail", {
+  } catch (error) {
+    return new Response("Error en el servidor", {
       status: 500,
     });
   }
-
-  if (!user) {
-    return new Response("Usuario no encontrado", {
-      status: 404,
-    });
-  }
-
-  const { data, error: orderError } = await supabase.from("orders").insert([
-    {
-      user_id: user.user.id,
-      date: new Date(),
-      order,
-    },
-  ]);
-
-  if (orderError) {
-    return new Response("Order Fail", {
-      status: 500,
-    });
-  }
-
-  if (!data) {
-    return new Response("Error al registrar el pedido", {
-      status: 500,
-    });
-  }
-
-  return new Response("Pedido registrado", {
-    status: 201,
-  });
 };
