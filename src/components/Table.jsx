2818b579885f0723bac0a150bb24cfdbd5d1fc23
adrioviedo <UO263569@uniovi.es>
2024-05-20 22:56:40 +0200

    Update Table component styling and structure

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,42 +1,44 @@
 export const Table = ({ products, total }) => {
-  <div className="overflow-x-auto">
-    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-      <thead className="text-xs text-gray-700 uppercase bg-gray-100  ">
-        <tr>
-          <th scope="col" className="px-6 py-3 rounded-s-lg">
-            Nombre
-          </th>
-          <th scope="col" className="px-6 py-3">
-            Qty
-          </th>
-          <th scope="col" className="px-6 py-3 rounded-e-lg">
-            Precio
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {products.map((item) => (
-          <tr className="bg-white " key={item.id}>
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-            >
-              {item.name}
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-[#E5CDA6] font-Roboto">
+        <thead className="text-md text-[#E5CDA6] uppercase bg-[#450408] font-Anton2 font-semibold">
+          <tr>
+            <th scope="col" className="px-6 py-3 rounded-tl-lg">
+              Nombre
             </th>
-            <td className="px-6 py-4">{item.quantity}</td>
-            <td className="px-6 py-4">{item.price}</td>
+            <th scope="col" className="px-6 py-3">
+              Qty
+            </th>
+            <th scope="col" className="px-6 py-3 rounded-tr-lg">
+              Precio
+            </th>
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <tr className="font-semibold text-gray-900 ">
-          <th scope="row" className="px-6 py-3 text-base">
-            Total
-          </th>
-          <td className="px-6 py-3"></td>
-          <td className="px-6 py-3">{total}</td>
-        </tr>
-      </tfoot>
-    </table>
-  </div>
+        </thead>
+        <tbody>
+          {products.map((item) => (
+            <tr className="bg-[#4504089e]" key={item.id}>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-[#E5CDA6] whitespace-nowrap"
+              >
+                {item.name}
+              </th>
+              <td className="px-6 py-4">{item.quantity}</td>
+              <td className="px-6 py-4">{item.price} €</td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot className="bg-[#450408ce] text-[#FFE6BB] font-semibold">
+          <tr>
+            <th scope="row" className="px-6 py-3 text-base rounded-bl-lg">
+              Total
+            </th>
+            <td className="px-6 py-3"></td>
+            <td className="px-6 py-3 rounded-br-lg">{total} €</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  )
 }
