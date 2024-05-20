79bfde6cd22b1f71510a6c829a27823618ce0d19
adrioviedo <UO263569@uniovi.es>
2024-05-20 22:51:58 +0200

    Update Aside component class names to use className attribute

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,20 +2,20 @@ import { useImg } from '../hooks/useImg'
 
 export const Aside = ({ categories, handleCategoryChange }) => {
   return (
-    <aside class="lg:w-[25%] w-full flex lg:flex-col gap-5 overflow-x-scroll lg:overflow-hidden pb-4 lg:pb-0">
+    <aside className="lg:w-[25%] w-full flex lg:flex-col gap-5 overflow-x-scroll lg:overflow-hidden pb-4 lg:pb-0">
       {categories.map((categoria) => (
         <article
-          class="relative  h-16 lg:h-24 min-w-20 bg-[#E5CDA6] rounded-xl overflow-hidden hover:bg-[#450408] hover:text-[#FFE6BB] cursor-pointer transition"
+          className="relative  h-16 lg:h-24 min-w-20 bg-[#E5CDA6] rounded-xl overflow-hidden hover:bg-[#450408] hover:text-[#FFE6BB] cursor-pointer transition"
           key={categoria.id}
           onClick={() => handleCategoryChange(categoria)}
         >
-          <h2 class="font-Anton text-sm lg:text-2xl absolute left-5 top-5 z-20">
+          <h2 className="font-Anton text-sm lg:text-2xl absolute left-5 top-5 z-20">
             {categoria.category}
           </h2>
           <img
             src={useImg(categoria.img)}
             alt=""
-            class="absolute object-fit object-center -right-6 -bottom-10 max-h-36"
+            className="absolute object-fit object-center -right-6 -bottom-10 max-h-36"
           />
         </article>
       ))}
